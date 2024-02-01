e5092891b0131d3f5f93e20810d4304878f778e0
HabiburRahman1 <habib.usa2014@gmail.com>
2024-02-01 19:35:36 +0900

    Error fixing

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app.secret_key = "super secret key"
 
 # Import the views public and private
 # import public views
-from views.public import quantum_gate_simulator 
+from views.public import quantum_programming
+from views.public import quantum_gate_simulator
+from views.public import quantum_historical_experiment_simulator
+from views.public import home
+
 app.run()
