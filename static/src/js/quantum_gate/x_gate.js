bc3e7806074c58448fb1d98abf69c7763f704476
HabiburRahman1 <habib.usa2014@gmail.com>
2024-02-27 01:20:09 +0900

    Gate simulator updated with new feature

--- static/src/js/quantum_gate/x_gate.js
+++ static/src/js/quantum_gate/x_gate.js
@@ -3,9 +3,21 @@ function allowDrop(ev) {
 }
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
+  // get the parent of the ev
+  var parent = ev.target.parentNode;
+  // get the id of the paren of the ev
+  var parent_id = parent.id;
+  // check if the parent id contain drop_place
+  if (parent_id.includes("drop_place")) {
+    parent.style.height = "20px";
+    parent.style.width = "20px";
+  }
 }
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
+  // change the style of the ev
+  ev.target.style.height = "60px";
+  ev.target.style.width = "60px";
   ev.target.appendChild(document.getElementById(data));
 }
