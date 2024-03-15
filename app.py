e9fb011fa3e8a3e913b94a473a32b8f13cf5597d
HabiburRahman1 <habib.usa2014@gmail.com>
2024-03-15 19:27:43 +0900

    Redirect to domain

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
@@ -17,3 +17,7 @@ from views import login_required_function
 from views import token
 from views.admin import login
 # app.run()
+@app.before_request
+def redirect_to_domain():
+    if request.host.startswith('3.89.197.36') and request.host.endswith(':5000'):
+        return redirect('https://quantum.qbithabib.com', code=301)
